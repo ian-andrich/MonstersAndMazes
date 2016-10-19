c16fad423f57c8d231776a775e915246b7c5ce09
Ian A <iandrich87@gmail.com>
2016-10-18 20:50:05 -0700

    Minor modifications.

--- character.pl
+++ character.pl
@@ -5,6 +5,7 @@ base_score(luck, 16).
 base_score(will, 13).
 base_score(grace, 11).
 
+% Begin defining derived Predicates.
 attribute(might).
 attribute(wits).
 attribute(skill).
@@ -14,23 +15,23 @@ attribute(grace).
 
 modifier(Attribute, Out_Mod) :-
   attribute(Attribute),
-  base_score(Attribute, Y),
-  modifier(Y, Out_Mod).
-modifier(1, -4).
-modifier(2, -4).
-modifier(3, -3).
-modifier(4, -3).
-modifier(5, -2).
-modifier(6, -2).
-modifier(7, -1).
-modifier(8, -1).
-modifier(X, 0) :- X >= 9, X =< 12.
-modifier(X, 1) :- X >= 13, X =< 14.
-modifier(X, 2) :- X >= 15, X =< 16.
-modifier(X, 3) :- X >=17, X =< 18.
-modifier(X, 4) :- X >= 19, X =< 20.
+  base_score(Attribute, Score),
+  modifier2(Score, Out_Mod).
+modifier2(1, -4).
+modifier2(2, -4).
+modifier2(3, -3).
+modifier2(4, -3).
+modifier2(5, -2).
+modifier2(6, -2).
+modifier2(7, -1).
+modifier2(8, -1).
+modifier2(X, 0) :- X >= 9, X =< 12.
+modifier2(X, 1) :- X >= 13, X =< 14.
+modifier2(X, 2) :- X >= 15, X =< 16.
+modifier2(X, 3) :- X >=17, X =< 18.
+modifier2(X, 4) :- X >= 19, X =< 20.
 
-:- discontiguous(score\2)
+:- discontiguous(score/2).
 score(melee_score, Score) :- melee_score(Score).
 melee_score(Score) :- 
   modifier(might, X), 
@@ -54,8 +55,7 @@ initiative_score(Score) :-
 score(defense_class, Score) :- defense_class(Score).
 defense_class(Score) :-
   modifier(luck, X),
-  % armor_modifier(Y),
-  Y = 0,
+  armor(Y),
   Score is 12 + X + Y.
 
 score(hits_total, Score) :- hits_total(Score).
@@ -65,10 +65,7 @@ hits_total(Score) :-
 
 score(effective_defense, Score) :- effective_defense(Score).
 effective_defense(Score) :-
-  % findall(X, armor(X), Bag),
-  % length(Bag, Y),
-  % Armor is 2 * Y,
-  Armor = 0,
+  armor(Armor),
   defense_class(Defense),
   modifier(wits, Wits),
   Score is Armor + Defense + Wits.
@@ -78,7 +75,7 @@ athletic_prowess(Score) :-
   modifier(might, Might),
   modifier(skill, Skill),
   modifier(luck, Luck),
-  Score is Might + Luck + Skill.
+  Score is Might + Skill + Luck.
 
 score(danger_evasion, Score) :- danger_evasion(Score).
 danger_evasion(Score) :-
@@ -114,11 +111,22 @@ thievery(Score) :-
   modifier(luck, Luck),
   Score is Wits + Luck.
 
+score(armor, Score) :- armor(Score).
+armor(Score) :-
+    Score is 0.
+
 d20_check(Type, Result) :-
   score(Type, Score),
   random_between(1, 20, Die),
-  Result is Score + Die.
+  Result is Score + Die, 
+    !,
+    format("Result was ~p!", Result).
 
-current_hits :-
-  hits_total(Score),
-  print(Score),
+d20_check(Type, Result, Threshold) :-
+    d20_check(Type, Result),
+     (Threshold =< Result,
+    format("~n Result successful."), !) |
+    format("~n Roll failed."), !.
+
+rollables(Bag) :- 
+    findall(X, score(X, _), Bag).
